425875ad69fe048946eb4a5d2550cfb7a0f9a783
PraveenYadav23 <praveencs0042@gmail.com>
2021-09-12 10:33:27 +0530

    initialize project and add simple rest api

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # springboot-docker-demo
+
+Steps:
+1. Create Spring boot Application
+2. Build a sample REST API
+3. Create Dockerfile
+4. Build Docker Image
+5. Run Docker Image in a container
---
+++ src/main/java/com/pavi/SpringbootDockerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.pavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Spring Boot Docker Demo";
+	}
+}
